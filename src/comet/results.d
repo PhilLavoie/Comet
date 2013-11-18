32ae5734d3f7aaa49c9f923b1bcac869129404ae
PhilLavoie <notarealaddress@secret.com>
2013-11-18 17:38:47 -0500

    Added some doc

--- src/comet/results.d
+++ src/comet/results.d
@@ -5,7 +5,10 @@ import comet.sma.segments;
 
 import std.container;
 
-
+/**
+  A structure built to represent a result. It holds the left segment
+  start index position, the segments length, and the cost of the calculation.  
+*/
 struct Result {
 
 private:
@@ -15,9 +18,11 @@ private:
   Cost _cost;
   
   this( size_t start, size_t length, Cost cost ) {
+  
     _start = start;
     _length = length;
     _cost = cost;
+    
   }
   
   @property void start( typeof( _start ) start ) { _start = start; }
@@ -31,18 +36,33 @@ public:
   @property auto cost() { return _cost; }
   
   int opCmp( Result rhs ) {
+  
+    //First compare on the cost criteria.
     if( _cost < rhs._cost - Cost.epsilon ) { 
+    
       return -1; 
+      
     } else if( rhs._cost + Cost.epsilon < _cost ) {
+    
       return 1;
+      
     }
-    //If the cost is equals, then the longer period wins.
+    
+    //If the cost is equals, then the longer segments length wins.
     auto cmp = rhs._length - _length;
     if( cmp ) { return cmp; }
-    //Arbitrary ordering otherwise.
+    
+    //Arbitrary ordering otherwise, based on left segment start.
     return _start - rhs._start;
+    
   }
   
+  /**
+    Because it isn't rare that two calculations done with floats
+    should give the exact same result but doesn't, this method is used
+    to compare two results on the basis of their cost but with an
+    epsilon parameter to be more flexible.
+  */
   bool isEquavalentTo( Result rhs, Cost epsilon = Cost.epsilon ) {
   
     return ( ( _cost - epsilon <= rhs._cost && rhs._cost + epsilon <= _cost ) );  
@@ -50,14 +70,20 @@ public:
   }
   
 }
+
+/**
+  Factory functions to create results.
+*/
 auto result( size_t start, size_t length, Cost cost ) {
   return Result( start, length, cost );
 }
+///Ditto.
 auto result( size_t start, SegmentsLength length, Cost cost ) {
   return result( start, length.value, cost );
 }
 
 unittest {
+
   Result r1, r2;
   r1.start = 0;
   r1.length = 100;
@@ -71,7 +97,7 @@ unittest {
   r1.cost = r2.cost + 1;
   assert( r2 < r1 );
   
-  //Period ordering.
+  //Length ordering.
   r2.cost = r1.cost;
   assert( r1 < r2 );
   r2.length = r1.length + 1;
@@ -87,6 +113,7 @@ unittest {
   //Equality.
   r1 = r2;
   assert( r1 == r2 );
+  
 }
 
 
@@ -99,6 +126,7 @@ unittest {
   then the latter is popped from the tree and the former is inserted, satisfying the limit.
 */
 struct Results {
+
   private RedBlackTree!( Result ) _results;
   private size_t _max;
 
@@ -109,8 +137,10 @@ struct Results {
     The parameter provided is that bound (inclusive).
   */
   this( size_t maxResults ) {
+  
     _results = new typeof( _results )();
     _max = maxResults;
+    
   }
   
   /**
@@ -126,30 +156,41 @@ struct Results {
         with the provided one.
   */
   void add( Result result ) {
+  
     if( !_max ) { return; }
     
     //Store result.
     if( _results.length < _max ) {
+    
       _results.insert( result );
+      
     //If we reached the maximum number of results, then we determine
     //if the current duplication result is better than the worst already known.
     //If so, we get rid of the worst and insert the better one.
     } else if( result < _results.back() ){
+    
       _results.removeBack();
       _results.insert( result );
+      
     }
+    
   }
 
   /**
     Returns a range of results in ascending order (the "lowest" result is the actual best).
   */
   auto opSlice() {
+  
     return _results[];
+    
   } 
+  
   /**
     Returns a range of results in ascending order (the "lowest" result is the actual best).
   */  
   auto range() {
+  
     return _results[];
+    
   }
 }
