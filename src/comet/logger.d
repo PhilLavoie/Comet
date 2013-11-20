8c3d5045924f10f5cda8e4b017981ff8a99195ab
PhilLavoie <notarealaddress@secret.com>
2013-11-20 17:28:45 -0500

    Added some doc to the logger module.

--- src/comet/logger.d
+++ src/comet/logger.d
@@ -1,3 +1,6 @@
+/**
+  Module offering facilities for automating the usage of verbosity with files.
+*/
 module comet.logger;
 
 import comet.meta;
@@ -6,12 +9,22 @@ import std.stdio;
 import std.algorithm;
 import std.typecons;
 
+/**
+  A logger is a wrapper around an output file that controls what gets sent to the file based on a
+  maximum verbosity accepted and a message verbosity (per print). Therefore, a logger who accepts
+  messages of verbosity up to 4 does not print any messages with a verbosity of 5 or higher.
+  
+  Ex:
+    auto logger = logger( stdout, 4 );
+    logger.logln( 4, "You see this" );
+    logger.log( 5, "But this does not get writtent );
+*/
 struct Logger {
 
 private:
 
-  File _out;
-  int _maxVerbosity;
+  File _out;          //The wrapped output.
+  int _maxVerbosity;  //The maximum verbosity accepted, inclusive.
     
   this( typeof( _out ) outFile, typeof( _maxVerbosity ) maxV ) {
   
@@ -22,6 +35,12 @@ private:
   
 public:
 
+  /**
+    Matches any call like write, writeln, writef, etc... that starts with write. The difference is
+    the expected calls here replace the "write" prefix with "log". So:
+      log.logln( 0, "blabla" ) calls writeln on the loggers wrapped file.
+      log.writeln does not compile.
+  */
   auto opDispatch( string s, T... )( int messageVerbosity, T args ) if( s.startsWith( "log" ) ) {
   
     if( messageVerbosity <= _maxVerbosity ) {
@@ -30,16 +49,13 @@ public:
     
     }
   
-  }
-  
-  auto opDispatch( string s, T... )( T args ) if( !s.startsWith( "log" ) ) {
-    
-    return mixin( identifier!_out ~ "." ~ s )( args );
-  
   }  
   
 }
 
+/**
+  Factory function for loggers.
+*/
 auto logger( T... )( T args ) {
 
   return Logger( args );
